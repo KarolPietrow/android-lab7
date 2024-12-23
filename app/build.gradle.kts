fba5102120dacc41399aa0599197718e7f08940e
Karol Pietrów <karolpietrow2004@gmail.com>
2024-12-24 00:53:17 +0100

    gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation (libs.okhttp)
+
 //    implementation(libs.androidx.lifecycle.runtime.ktx.v260)
 
 
